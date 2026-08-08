578f3af0ae30fce28131e447b17dfb9e7851a9ac
Cyril Auquier <cyrilauquier@hotmail.fr>
2026-08-08 10:57:44 +0200

    add test controller

---
+++ src/Inkukan.Api/Controllers/v1/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inkukan.Api.Controllers.v1;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    public IActionResult Get(string? str)
+    {
+        return Ok(str.Length);
+    }
+}
