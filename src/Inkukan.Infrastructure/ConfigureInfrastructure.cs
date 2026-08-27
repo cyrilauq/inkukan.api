9a7ea264728b5425f19b845400326e0b8f405049
Cyril Auquier <cyrilauquier@hotmail.fr>
2026-08-27 19:54:05 +0200

    remove duplicate injection

--- src/Inkukan.Infrastructure/ConfigureInfrastructure.cs
+++ src/Inkukan.Infrastructure/ConfigureInfrastructure.cs
@@ -45,7 +45,6 @@ public static class ConfigureInfrastructure
             .AddScoped<IBaseRepository<MangaCollection>, CollectionRepository>()
             .AddScoped<IBaseRepository<UserListItem>, BaseRepository<UserListItem>>()
             .AddScoped<IBaseRepository<User>, BaseRepository<User>>()
-            .AddScoped<IBaseRepository<UserListItem>, BaseRepository<UserListItem>>()
             .AddScoped<IBlobStorage, VercelBlobStorage>();
 
         return services;
